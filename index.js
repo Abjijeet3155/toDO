df2df9f161a45efb6f9b669f95f10d3c61dd2dee
unknown <abjijeet.kumar.ds.2021@mitmeerut.ac.in>
2024-09-01 06:17:05 +0530

    First commit

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const app = require('./app');
 const db=require('./config/db')
 const userModel=require('./model/user.model')
 const TodoModel= require('./model/todo.model')
-const port = 3000;
+const port = 9000;
 app.get('/',(req,res)=>{
 res.send("Hello World!!!!!")
 });
 
-app.connect(port, () => {
+app.listen(port, () => {
     console.log(`Connected to server  http://localhost:${port}`);
-});
+}).on('error',(err)=>{
+    if (err.code== 'EADDRINUSE') {
+        console.error(`Port ${port} is already in use`);
+        
+    }else{
+        console.err(err);
+    }
+})
     
